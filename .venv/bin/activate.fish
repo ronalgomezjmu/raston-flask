c0b4fde2b00b1db9dccf11fbcd3370d7dd2df039
VanFossen <nvanfossen@gmail.com>
2022-07-21 11:07:24 -0400

    fixed venv

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vanfossen/flask_radis/.venv"
+set -gx VIRTUAL_ENV "/home/vanfossen/dev/raston-flask/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
